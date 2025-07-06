232d0f8e1f47b8769a19dc0551a7a3c5511abd47
chubukinandrew <cubukinandrej449@gmail.com>
2025-07-06 17:44:49 +0400

    Add backend folder and update .gitignore

---
+++ todo-backend/index.js
@@ -0,0 +1,48 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: '*', // разрешаем запросы с любых источников (для теста)
+}));
+app.use(express.json());
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // для Railway/Heroku
+  },
+});
+
+app.get('/tasks', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM tasks ORDER BY id DESC');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.post('/tasks', async (req, res) => {
+  try {
+    const { title } = req.body;
+    const result = await pool.query(
+      'INSERT INTO tasks (title) VALUES ($1) RETURNING *',
+      [title]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
