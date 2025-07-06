e49052a63534ef86de35485b8274f1af16269e25
chubukinandrew <cubukinandrej449@gmail.com>
2025-07-06 19:10:18 +0400

    Fix: remove react-scripts from backend

--- todo-backend/index.js
+++ todo-backend/index.js
@@ -5,7 +5,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: '*', // разрешаем запросы с любых источников (для теста)
